3d656e1023c6f425713546b180a800697bdbe5cc
chris aquino <chris@radishmouse.com>
2019-05-07 14:39:00 -0400

    moves redux code to own file, uses map state to props

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -4,7 +4,7 @@ import React from 'react';
 function PokeList({cards, handleClick}) {
 
     const cardItems = cards.map(card => {
-        return <li onClick={() => {
+        return <li key={card.id} onClick={() => {
             // to pass handleClick a custom argument,
             // we must wrap it in an anonymous function.
             handleClick(card.id);
